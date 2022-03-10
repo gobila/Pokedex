a00a1c2eb375e5f2c82beea584814567936eea1a
Moises <12902689+gobila@users.noreply.github.com>
2022-03-10 16:54:57 -0300

    create theme base

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import apiConnect from './service/apiConnect';
 
+import './theme/global.scss';
+
 function App() {
   const [types, setTypes] = useState();
   const [isLoading, setIsLoading] = useState(true);
@@ -35,24 +37,6 @@ function App() {
   }, [isLoading]);
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
 
       {pokemonData.map((item) => (
         <div key={item.id}>
@@ -60,7 +44,7 @@ function App() {
             src={item.sprites.front_default}
             alt={`${item.name} srite`}
           />
-          <p>{item.name}</p>
+          <p className="poppins-s16">{item.name}</p>
           <p>{item.types.map((i) => <p>{i.type.name}</p>)}</p>
           <p>{item.order}</p>
         </div>
