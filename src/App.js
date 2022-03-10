3d83e8e38eff6ecb89778a635751f8e2709ee397
Moises <12902689+gobila@users.noreply.github.com>
2022-03-10 14:27:09 -0300

    create a connection in app page

--- src/App.js
+++ src/App.js
@@ -6,22 +6,33 @@ import apiConnect from './service/apiConnect';
 
 function App() {
   const [types, setTypes] = useState();
+  const [isLoading, setIsLoading] = useState(true);
   const [pokemonData, setPokemonData] = useState([]);
   const [pokemonList, setPokemonList] = useState([]);
   const connect = apiConnect;
 
+  async function getAllPokemon() {
+    const getAll = await connect.getAll();
+    setPokemonList(getAll.results);
+    setIsLoading(false);
+  }
   async function getPokemon() {
+    getAllPokemon();
     const list = pokemonList.map(async (item) => connect.getPokemon(item.name));
     const results = await Promise.all(list);
     setPokemonData(results);
   }
+  async function getTypes() {
+    getAllPokemon();
+    const typesData = await connect.getTypes();
+    setTypes(typesData.results);
+    console.log(typesData.results);
+  }
 
   useEffect(async () => {
-    const getAll = await connect.getAll();
-    setPokemonList(getAll.results);
     getPokemon();
-  }, []);
-
+    getTypes();
+  }, [isLoading]);
   return (
     <div className="App">
       {/* <header className="App-header">
@@ -44,7 +55,11 @@ function App() {
       </header> */}
 
       {pokemonData.map((item) => (
-        <div>
+        <div key={item.id}>
+          <img
+            src={item.sprites.front_default}
+            alt={`${item.name} srite`}
+          />
           <p>{item.name}</p>
           <p>{item.types.map((i) => <p>{i.type.name}</p>)}</p>
           <p>{item.order}</p>
